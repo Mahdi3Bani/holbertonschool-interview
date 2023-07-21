3f391e25c7bcbdde800ef3fb8fb9b22cce23a5ff
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-07-22 00:54:16 +0200

    rain

---
+++ rain/0-rain.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def rain(walls):
+
+    n = len(walls)
+  
+    
+    res = 0
+  
+    for i in range(1, n - 1):
+  
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+  
+        right = walls[i]
+  
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
+  
+        res = res + (min(left, right) - walls[i])
+  
+    return res
